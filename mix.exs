48d22658074573886c16f6a77bd2580162de95f8
Rhys <rhys@wavebox.io>
2024-02-27 13:47:25 +0000

    Update API endpoints to reflect Feb 26, 2024 Mistral changes

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MistralClient.MixProject do
   def project do
     [
       app: :mistral_client,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11",
       description: description(),
       start_permanent: Mix.env() == :prod,
